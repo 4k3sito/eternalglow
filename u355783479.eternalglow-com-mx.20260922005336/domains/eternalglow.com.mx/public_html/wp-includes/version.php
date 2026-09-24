1a21a9d08da075b52730d11596f6e6c102f1a57f
4k3sito <akexanderr123@gmail.com>
2026-09-24 02:31:51 -0600

    Sincroniza con producción: WP 7.1.2, Rank Math 1.0.279, Docket Cache 26.04.06
    Alineado con GoDaddy (public_html). Excluye wp-config, cachés, backups
    WPvivid y mu-plugins/local-dev.php (solo entorno local).

--- u355783479.eternalglow-com-mx.20260922005336/domains/eternalglow.com.mx/public_html/wp-includes/version.php
+++ u355783479.eternalglow-com-mx.20260922005336/domains/eternalglow.com.mx/public_html/wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.1.1';
+$wp_version = '7.1.2';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
@@ -56,4 +56,4 @@ $required_php_extensions = array(
  */
 $required_mysql_version = '5.5.5';
 
-$wp_local_package = 'es_MX';
+$wp_local_package = 'ar';
